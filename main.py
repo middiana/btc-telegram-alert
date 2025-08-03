2fe1186fb6c0300b6f82a612190b4e25040f80cb
middiana <koijkl12@gmail.com>
2025-08-03 23:21:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("✅ main.py 정상 실행됨")
+print("✅ main.py 시작")
+
+try:
+    from strategy import check_long_signal
+    print("✅ strategy 모듈 import 성공")
+except Exception as e:
+    print(f"❌ strategy import 실패: {e}")
