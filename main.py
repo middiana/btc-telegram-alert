0c4dd18cc26865440ba434a230ccb139371afb75
middiana <koijkl12@gmail.com>
2025-08-04 00:11:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+
 from strategy import check_long_signal
 from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 from utils import send_telegram_message
-import time
 
 print("✅ main.py 시작")
+print("✅ strategy 모듈 import 성공")
 
-if __name__ == "__main__":
-    print("✅ strategy 모듈 import 성공")
-    signal = check_long_signal()
-    print(f"📦 check_long_signal 결과: {signal}")
-    if signal:
-        send_telegram_message(TELEGRAM_TOKEN, TELEGRAM_CHAT_ID, signal)
+signal = check_long_signal()
+
+if signal:
+    send_telegram_message(TELEGRAM_TOKEN, TELEGRAM_CHAT_ID, f"📢 신호 발생: {signal}")
+else:
+    print("📭 신호 없음 또는 데이터 오류")
