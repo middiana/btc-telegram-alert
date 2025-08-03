12a19c6910a55183aba17f84b39536d53b9303c3
middiana <koijkl12@gmail.com>
2025-08-03 16:06:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from strategy import check_signal
+import time
 
 if __name__ == "__main__":
-    check_signal()
+    print("🚀 영빈 선물전략 v1.2 실행 시작")
+
+    signal = check_signal()
+    if signal:
+        print("✅ 진입 조건 만족. 텔레그램 알림 전송됨.")
+    else:
+        print("❌ 진입 조건 미충족.")
