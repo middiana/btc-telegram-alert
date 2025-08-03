7244d7dc7af2a9e598c3335589318e4622bac527
middiana <koijkl12@gmail.com>
2025-08-03 23:37:20 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
-SYMBOL = "BTCUSDT_UMCBL"   # Bitget 선물 심볼
-INTERVAL = "15m"           # 봉 간격: 15분봉
+SYMBOL = "ETHUSDT_UMCBL"   # 일단 ETH로 교체해서 테스트
+INTERVAL = "15m"
 
 TELEGRAM_TOKEN = "8454656493:AAGjqH4zt2Mn-HBleMtCrFgsXLwModMDbC8"
 TELEGRAM_CHAT_ID = "7426355357"
