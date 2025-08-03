a8e775920269e083fa4e8e87d045159f66d9432a
middiana <koijkl12@gmail.com>
2025-08-03 23:31:42 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
+# config.py
+
+SYMBOL = "BTCUSDT_UMCBL"   # Bitget 선물 심볼
+INTERVAL = "15m"           # 봉 간격: 15분봉
+
 TELEGRAM_TOKEN = "8454656493:AAGjqH4zt2Mn-HBleMtCrFgsXLwModMDbC8"
 TELEGRAM_CHAT_ID = "7426355357"
-SYMBOL = "BTCUSDT"
-INTERVAL = "15m"
